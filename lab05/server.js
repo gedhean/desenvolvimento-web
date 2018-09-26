c8bcf08e30d4267b80c1ad9f36ab95d2e62a4994
gedhean <gedhean.dev@gmail.com>
2018-09-26 15:35:04 -0300

    faild to get css

--- lab05/server.js
+++ lab05/server.js
@@ -10,7 +10,7 @@ const app = express()
 const fs = require("fs");
 const path = require('path');
 
-app.use(express.static(path.join( __dirname + '/public')));
+app.use('/', express.static(path.join( __dirname + '/public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname + '/views'))
 app.set('view engine', 'ejs')
