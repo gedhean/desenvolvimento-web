e5db1bdffd633b88ce1c037a7d97baf2217d3102
gedhean <gedhean.dev@gmail.com>
2018-10-03 19:51:20 -0300

    Pratica 06 terminada

--- lab05/server.js
+++ lab05/server.js
@@ -22,51 +22,33 @@ app.get('/users/:userId/books/:bookId', function(req, res) {
 app.get('/', function(req, res) {
   fs.readdir(path.resolve(__dirname + '/public/users-data'), (err, data) => {
     if (err) {
-      res.end(err)
+      res.render('404')
     }
     res.render('index', { welcome: 'Welcome to the CV land', users: data })
   })
 })
 
-app.get('/cv/:usu', function(req, res) {
-  var arr1 = [],
-    arr2 = []
-  var diret
-  diret = path.join(__dirname + '/public/data/' + req.params.usu)
-  var dadosCV = {
-    userName: req.params.usu,
-    linesSec1: [],
-    linesSec2: []
+app.get('/cv', function(req, res) {
+  const user = req.query.user
+  const users = fs.readdirSync(path.resolve(__dirname + '/public/users-data'))
+
+  if (users.includes(user)) {
+    fs.readFile(
+      path.resolve(`${__dirname}/public/users-data/${user}/data.json`),
+      (err, userData) => {
+        if (err) {
+          res.render('404')
+          console.log(err)
+        }
+        res.render('cv', JSON.parse(userData))
+        console.log(req.query, JSON.parse(userData))
+      }
+    )
+  } else {
+    res.render('404')
   }
-
-  // Leitura dos dados da 1a secao
-  fs.readFile(diret + '/s1.txt', function(err, data) {
-    if (err) {
-      return console.error(err)
-    }
-    dadosCV.linesSec1 = data.toString().split('\n')
-  })
-
-  // Leitura dos dados da 2a secao
-  fs.readFile(diret + '/s2.txt', function(err, data) {
-    if (err) {
-      return console.error(err)
-    }
-    dadosCV.linesSec2 = data.toString().split('\n')
-
-    // executa cv.ejs
-    res.render('cv', dadosCV)
-  })
+  // executa cv.ejs
 })
-
-/*
-app.get('/rec/:fName', function (req, res) {
-	var filename = "public/"+req.params.fName;
-	console.log('Enviando arquivo '+filename);
-	res.sendFile(
-	   path.join(__dirname+'/'+filename)); 
-});
-*/
 
 app.listen(8080, function() {
   console.log('Example app listening on port 8080!')
